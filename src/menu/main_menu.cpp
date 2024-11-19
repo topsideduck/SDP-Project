5f08b995a11aebad34f1f497dc4de0767ad3bab5
Harikeshav R <r.harikeshav@icloud.com>
2024-11-19 16:54:16 -0500

    Create main menu class

---
+++ src/menu/main_menu.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Harikeshav R on 11/19/24.
+//
+
+#include "src/menu/main_menu.h"
+
+namespace game
+{
+    MainMenu::MainMenu() = default;
+
+    MainMenu::~MainMenu() = default;
+} // game
---
+++ src/menu/main_menu.h
@@ -0,0 +1,18 @@
+//
+// Created by Harikeshav R on 11/19/24.
+//
+
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+namespace game
+{
+    class MainMenu
+    {
+        MainMenu();
+
+        ~MainMenu();
+    };
+} // game
+
+#endif //MAIN_MENU_H
