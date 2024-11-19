20d83a551a82f5defbed3a0697d37241b38d8dda
Harikeshav R <r.harikeshav@icloud.com>
2024-11-19 18:21:57 -0500

    Create a header file that defines all the constants required for the program

---
+++ src/util/constants.h
@@ -0,0 +1,29 @@
+//
+// Created by Harikeshav R on 11/19/24.
+//
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAIN_MENU_IMAGE_FILE_PATH "assets/main_menu.png"
+#define QUIT_MENU_IMAGE_FILE_PATH "assets/quit_menu.png"
+
+// Define details of the quit button on the main menu
+#define MAIN_MENU_QUIT_BUTTON_X_COORDINATE 80
+#define MAIN_MENU_QUIT_BUTTON_Y_COORDINATE 195
+#define MAIN_MENU_QUIT_BUTTON_X_SIZE 160
+#define MAIN_MENU_QUIT_BUTTON_Y_SIZE 25
+
+// Define details of the yes button on the quit menu
+#define QUIT_MENU_YES_BUTTON_X_COORDINATE 80
+#define QUIT_MENU_YES_BUTTON_Y_COORDINATE 125
+#define QUIT_MENU_YES_BUTTON_X_SIZE 160
+#define QUIT_MENU_YES_BUTTON_Y_SIZE 25
+
+// Define details of the no button on the quit menu
+#define QUIT_MENU_NO_BUTTON_X_COORDINATE 80
+#define QUIT_MENU_NO_BUTTON_Y_COORDINATE 165
+#define QUIT_MENU_NO_BUTTON_X_SIZE 160
+#define QUIT_MENU_NO_BUTTON_Y_SIZE 25
+
+#endif //CONSTANTS_H
