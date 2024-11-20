dda7212088a708704bf598b47c2f456e8a2e69fb
Harikeshav R <r.harikeshav@icloud.com>
2024-11-20 17:04:17 -0500

    Add sample code from the documentation of tigr.h to access the keyboard from the game window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
+#include <iostream>
+
 #include "src/lib/FEHLCD.h"
 #include "src/menu/main_menu.h"
+#include "src/lib/tigr.h"
+
 
 [[noreturn]] int main()
 {
-    // LCD.WriteLine("Hello World!");
     game::MainMenu::draw_main_menu();
-    while (true)
-    {
-        LCD.Update();
-        // Never end
-    }
+    // while (true)
+    // {
+    //     // Read the keyboard and move the player.
+    //     // if (tigrKeyDown(LCD.screen, TK_SPACE))
+    //     //     std::cout << "space" << std::endl;
+    //     // // if (tigrKeyHeld(screen, TK_LEFT) || tigrKeyHeld(screen, 'A'))
+    //     // //     playerxs -= 10;
+    //     // // if (tigrKeyHeld(screen, TK_RIGHT) || tigrKeyHeld(screen, 'D'))
+    //     // //     playerxs += 10;
+    //     LCD.Update();
+    //     // Never end
+    // }
 }
