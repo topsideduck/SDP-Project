8edf0423b7379f195a4417df57bd1c886a02fe1e
Harikeshav R <r.harikeshav@icloud.com>
2024-11-29 13:13:44 -0500

    main.cpp now handles all click events and draws menus accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,78 @@
-#include <iostream>
+// #include <iostream>
 
-#include "src/lib/FEHLCD.h"
+// #include "src/lib/FEHLCD.h"
+#include "src/menu/controls_menu.h"
+#include "src/menu/credits_menu.h"
+#include "src/menu/instructions_menu.h"
 #include "src/menu/main_menu.h"
-#include "src/lib/tigr.h"
+#include "src/menu/menus.h"
+#include "src/menu/play_game_menu.h"
+#include "src/menu/quit_menu.h"
+#include "src/menu/statistics_menu.h"
+
+
+// #include "src/lib/tigr.h"
 
 
 [[noreturn]] int main()
 {
-    game::MainMenu::draw_main_menu();
+    Menus current_menu = Menus::MainMenu;
+
+    while (true)
+    {
+        switch (current_menu)
+        {
+            case Menus::ControlsMenu:
+            {
+                game::ControlsMenu::draw_controls_menu();
+                current_menu = game::ControlsMenu::handle_controls_menu_input();
+                break;
+            }
+
+            case Menus::CreditsMenu:
+            {
+                game::CreditsMenu::draw_credits_menu();
+                current_menu = game::CreditsMenu::handle_credits_menu_input();
+                break;
+            }
+
+            case Menus::InstructionsMenu:
+            {
+                game::InstructionsMenu::draw_instructions_menu();
+                current_menu = game::InstructionsMenu::handle_instructions_menu_input();
+                break;
+            }
+
+            case Menus::MainMenu:
+            {
+                game::MainMenu::draw_main_menu();
+                current_menu = game::MainMenu::handle_main_menu_input();
+                break;
+            }
+
+            case Menus::PlayGameMenu:
+            {
+                game::PlayGameMenu::draw_play_game_menu();
+                current_menu = game::PlayGameMenu::handle_play_game_menu_input();
+                break;
+            }
+
+            case Menus::QuitMenu:
+            {
+                game::QuitMenu::draw_quit_menu();
+                current_menu = game::QuitMenu::handle_quit_menu_input();
+                break;
+            }
+
+            case Menus::StatisticsMenu:
+            {
+                game::StatisticsMenu::draw_statistics_menu();
+                current_menu = game::StatisticsMenu::handle_statistics_menu_input();
+                break;
+            }
+        }
+    }
+
     // while (true)
     // {
     //     // Read the keyboard and move the player.
