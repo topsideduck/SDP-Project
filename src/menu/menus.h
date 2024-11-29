0ff972d98e463054831884ce47cd676a20fc8b12
Harikeshav R <r.harikeshav@icloud.com>
2024-11-29 12:16:03 -0500

    Create Menus enum

---
+++ src/menu/menus.h
@@ -0,0 +1,18 @@
+//
+// Created by Harikeshav R on 11/29/24.
+//
+
+#ifndef MENUS_H
+#define MENUS_H
+
+enum Menus{
+    ControlsMenu,
+    CreditsMenu,
+    InstructionsMenu,
+    MainMenu,
+    PlayGameMenu,
+    QuitMenu,
+    StatisticsMenu,
+};
+
+#endif //MENUS_H
