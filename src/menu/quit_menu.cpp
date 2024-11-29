e16e5844c81b9aba6e2789c02fab604936a39079
Harikeshav R <r.harikeshav@icloud.com>
2024-11-29 13:13:07 -0500

    Rewrite quit menu to prevent infinite recursion

--- src/menu/quit_menu.cpp
+++ src/menu/quit_menu.cpp
@@ -5,7 +5,7 @@
 #include "src/menu/quit_menu.h" // Include the header for the QuitMenu class
 
 #include "src/lib/FEHImages.h"   // For handling images using FEHImage
-#include "src/menu/main_menu.h"  // For accessing main menu functionality
+#include "src/menu/menus.h"      // Include the Menus enum
 #include "src/util/constants.h"  // For constants like file paths and button coordinates
 #include "src/util/input.h"      // For getting touch input from the user
 
@@ -38,10 +38,10 @@ namespace game
 
         // Update the LCD display to show the menu image
         LCD.Update();
+    }
 
-        // Flags to track whether the user clicked on the "Quit" or "Stay" button
-        bool quit_game_clicked = false, open_main_menu_clicked = false;
-
+    Menus QuitMenu::handle_quit_menu_input()
+    {
         // Monitor user input in an infinite loop
         while (true)
         {
@@ -54,8 +54,7 @@ namespace game
                 y_coordinate >= QUIT_MENU_STAY_BUTTON_Y_COORDINATE &&
                 y_coordinate <= QUIT_MENU_STAY_BUTTON_Y_COORDINATE + QUIT_MENU_STAY_BUTTON_Y_SIZE)
             {
-                open_main_menu_clicked = true;
-                break; // Exit the loop if "Stay" is clicked
+                return Menus::MainMenu;
             }
 
             // Check if the user clicked the "Quit" button
@@ -64,31 +63,9 @@ namespace game
                 y_coordinate >= QUIT_MENU_QUIT_BUTTON_Y_COORDINATE &&
                 y_coordinate <= QUIT_MENU_QUIT_BUTTON_Y_COORDINATE + QUIT_MENU_QUIT_BUTTON_Y_SIZE)
             {
-                quit_game_clicked = true;
-                break; // Exit the loop if "Quit" is clicked
+                quit_game();
             }
         }
-
-        // If the "Quit" button was clicked, exit the game
-        if (quit_game_clicked)
-        {
-            quit_game();
-        }
-        // If the "Stay" button was clicked, return to the main menu
-        else if (open_main_menu_clicked)
-        {
-            open_main_menu();
-        }
-    }
-
-    /**
-     * @brief Opens the main menu by calling its draw method.
-     *
-     * This method transitions the screen from the quit menu to the main menu.
-     */
-    void QuitMenu::open_main_menu()
-    {
-        MainMenu::draw_main_menu();
     }
 
     /**
