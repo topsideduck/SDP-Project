2a0111a6c772f534d4cd0e66c48462920e97ca14
Harikeshav R <r.harikeshav@icloud.com>
2024-11-30 14:02:12 -0500

    Create ArrowManager class to manage every arrow in frame

---
+++ src/gameplay/arrow_manager.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Harikeshav R on 11/30/24.
+//
+
+#include "src/gameplay/arrow_manager.h"
+#include "src/gameplay/arrow.h"
+
+#include <random>
+
+
+namespace game
+{
+    ArrowManager::ArrowManager() = default;
+
+    ArrowManager::~ArrowManager() = default;
+
+} // game
---
+++ src/gameplay/arrow_manager.h
@@ -0,0 +1,40 @@
+//
+// Created by Harikeshav R on 11/30/24.
+//
+
+#ifndef ARROW_MANAGER_H
+#define ARROW_MANAGER_H
+
+#include "src/gameplay/arrow.h"
+
+#include "random"
+#include "vector"
+
+
+namespace game
+{
+    class ArrowManager
+    {
+    private:
+        std::vector<Arrow *> arrows;
+
+        std::random_device random_device;
+        std::mt19937 random_number_generator{random_device()};
+        std::uniform_int_distribution<std::mt19937::result_type> distribution_4{1, 4};
+
+    public:
+        ArrowManager();
+
+        ~ArrowManager();
+
+        void create_random_arrow();
+
+        void move_arrows(int delta_x, int delta_y) const;
+
+        void delete_arrows();
+
+        void draw_all_arrows() const;
+    };
+} // game
+
+#endif //ARROW_MANAGER_H
