708a3c72c1d706327df36332dc234ba3eda4f5a4
Harikeshav R <r.harikeshav@gmail.com>
2024-12-01 16:20:39 -0500

    Update documentation for Menus enum

--- src/menu/menus.h
+++ src/menu/menus.h
@@ -1,21 +1,24 @@
-//
-// Created by Harikeshav R on 11/29/24.
-//
-
 #ifndef MENUS_H
 #define MENUS_H
 
+/**
+ * @enum Menus
+ * @brief Represents the various menu states in the game.
+ *
+ * This enumeration is used to identify and transition between different
+ * menus in the game, such as the main menu, controls menu, and others.
+ */
 enum Menus
 {
-    ControlsMenu,
-    CreditsMenu,
-    InstructionsMenu,
-    MainMenu,
-    MusicPickerMenu,
-    PlayGameMenu,
-    QuitMenu,
-    ScoreMenu,
-    StatisticsMenu,
+    ControlsMenu, /**< Represents the controls menu. */
+    CreditsMenu, /**< Represents the credits menu. */
+    InstructionsMenu, /**< Represents the instructions menu. */
+    MainMenu, /**< Represents the main menu. */
+    MusicPickerMenu, /**< Represents the music picker menu. */
+    PlayGameMenu, /**< Represents the play game menu. */
+    QuitMenu, /**< Represents the quit menu. */
+    ScoreMenu, /**< Represents the score menu. */
+    StatisticsMenu /**< Represents the statistics menu. */
 };
 
-#endif //MENUS_H
+#endif // MENUS_H
