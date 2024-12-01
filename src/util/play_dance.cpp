3d862a6b8c66fa411d0d195673fb7963854e0e03
Harikeshav R <r.harikeshav@gmail.com>
2024-12-01 11:55:01 -0500

    Create documentation for play dance

--- src/util/play_dance.cpp
+++ src/util/play_dance.cpp
@@ -1,20 +1,27 @@
-//
-// Created by Harikeshav R on 11/30/24.
-//
+#include "src/util/play_dance.h" // Include header for play_dance functionality
+#include <filesystem>            // For handling file paths
+#include "src/gameplay/arrow.h"  // Include ArrowDirection enum
+#include "src/util/constants.h"  // Include dance-related constants
 
-#include "src/util/play_dance.h"
-
-#include "src/gameplay/arrow.h"
-#include "src/util/constants.h"
-
-#include "filesystem"
-
-
+/**
+ * @brief Displays a dance animation based on the specified direction and updates the animation frame.
+ *
+ * This function selects and renders the appropriate dance frame for the given direction.
+ * The frame updates according to the `dance_counter` and the specified update rate. Once all frames
+ * are displayed, the counter resets to loop the animation.
+ *
+ * @param direction The direction for which the dance animation should be played (e.g., Left, Right, Up, etc.).
+ * @param dance_counter Pointer to the frame counter controlling the animation progress.
+ * @param x_coordinate X-coordinate for drawing the frame on the screen.
+ * @param y_coordinate Y-coordinate for drawing the frame on the screen.
+ * @param update_rate The rate at which the animation frames are updated.
+ */
 void play_dance(const ArrowDirection direction, int *dance_counter, int x_coordinate, int y_coordinate, int update_rate)
 {
     std::string dance_frame_base_file_name;
     int number_of_dance_frames{};
 
+    // Determine the base filename and number of frames for the given direction
     switch (direction)
     {
         case (ArrowDirection::Left):
@@ -51,16 +58,20 @@ void play_dance(const ArrowDirection direction, int *dance_counter, int x_coordi
         {
             dance_frame_base_file_name = DANCE_SITTING_BASE_FILE_NAME;
             number_of_dance_frames = DANCE_SITTING_NUMBER_OF_FRAMES;
+            break;
         }
     }
 
-    const std::filesystem::path dance_frame_file = dance_frame_base_file_name + std::to_string(
-                                                       (*dance_counter) / update_rate + 1) + ".png";
+    // Construct the file path for the current dance frame
+    const std::filesystem::path dance_frame_file = dance_frame_base_file_name +
+                                                   std::to_string((*dance_counter) / update_rate + 1) + ".png";
     const std::filesystem::path result = DANCE_FRAMES_PATH / dance_frame_file;
 
+    // Load and draw the dance frame
     FEHImage dance_frame{result.string().c_str()};
     dance_frame.Draw(x_coordinate, y_coordinate);
 
+    // Increment the counter and reset if the animation completes
     (*dance_counter)++;
 
     if (*dance_counter >= update_rate * number_of_dance_frames)
--- src/util/play_dance.h
+++ src/util/play_dance.h
@@ -1,12 +1,18 @@
-//
-// Created by Harikeshav R on 11/30/24.
-//
-
 #ifndef PLAY_DANCE_H
 #define PLAY_DANCE_H
 
-#include "src/gameplay/arrow.h"
+#include "src/gameplay/arrow.h" // Include ArrowDirection enum
 
-void play_dance(const ArrowDirection direction, int *dance_counter, int x_coordinate, int y_coordinate, int update_rate);
+/**
+ * @brief Renders a dance animation for the given direction.
+ *
+ * @param direction The direction of the dance animation (e.g., Left, Up, etc.).
+ * @param dance_counter Pointer to the counter controlling animation frames.
+ * @param x_coordinate X-coordinate for the frame's position on the screen.
+ * @param y_coordinate Y-coordinate for the frame's position on the screen.
+ * @param update_rate Frame update rate controlling the speed of the animation.
+ */
+void play_dance(const ArrowDirection direction, int *dance_counter, int x_coordinate, int y_coordinate,
+                int update_rate);
 
-#endif //PLAY_DANCE_H
+#endif // PLAY_DANCE_H
